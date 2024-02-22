7f1d2ba5d277e04dd252da93f8bf36bc2e1ac282
Sam Watts <SamWattsJ@outlook.com>
2024-02-22 13:04:53 +0000

    Camel case BAD
    return to convention, changed a lot of variable names.

--- This File Is A Mess.py
+++ This File Is A Mess.py
@@ -2,28 +2,28 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-class radioactiveDecay:
-    def __init__(self, N, lam):
-        self.t = 0.0
-        self.N = N
-        self.lam = lam
-        self.halfLife = math.log(2.0)/self.lam
-        self.dt = 0.1*self.halfLife
+class RadioactiveDecay:
+    def __init__(self, _number_of_particles, _decay_constant):
+        self.time = 0.0
+        self.number_of_particles = _number_of_particles
+        self.decay_constant = _decay_constant
+        self.half_life = math.log(2.0)/self.decay_constant
+        self.dt = 0.1*self.half_life
 
     def calculate(self, dt=None):
         _dt = self.dt
         if dt is not None:
             _dt = dt
-        self.N -= self.lam*self.N*_dt
+        self.number_of_particles -= self.decay_constant * self.number_of_particles * _dt
         self.t += _dt
 
-decay = radioactiveDecay(1e6, math.log(2.0)/2)
+decay = RadioactiveDecay(1e6, math.log(2.0)/2)
 N = [1e6]
-t = [0.0]
-num = int(decay.halfLife*5/decay.dt)
+time = [0.0]
+num = int(decay.half_life*5/decay.dt)
 for i in range(1, num):
     decay.calculate()
-    N.append(decay.N)
-    t.append(decay.t)
+    N.append(decay.number_of_particles)
+    time.append(decay.time)
 plt.semilogy(t, N)
 plt.show()
